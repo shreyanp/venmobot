4d2daf40d2ab6f04bea0059462640380cead81bf
Shreyan Purwar <113260208+shreyanp@users.noreply.github.com>
2023-08-19 04:17:27 -0400

    Update venmoDriver.py

--- venmoDriver.py
+++ venmoDriver.py
@@ -23,6 +23,11 @@ def loginVenmo(number1, password1):
     options.add_experimental_option("detach", True)
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     #number1 = "6126668708"
