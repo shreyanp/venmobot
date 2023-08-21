032e4cfe3008ac4feadb514eaeda591a1a17ab69
Shreyan Purwar <113260208+shreyanp@users.noreply.github.com>
2023-08-21 03:32:31 -0400

    Update venmoDriver.py

--- venmoDriver.py
+++ venmoDriver.py
@@ -21,7 +21,6 @@ def waitTillDate(spec_date, spec_time):
 
 def loginVenmo(number1, password1):
     options = webdriver.FirefoxOptions()
-    options.add_experimental_option("detach", True)
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
